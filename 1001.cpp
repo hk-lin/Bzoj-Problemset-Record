07f5c2e4ff6ccf0d11f23c82e7830ce95b987d35
hk-lin <464453990@qq.com>
2017-11-13 13:46:11 +0800

    4034

---
+++ 1001.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#define clr(x) memset(x,0,sizeof(x))
+#define clr_1(x) memset(x,-1,sizeof(x))
+#define mod 1000000007
+#define LL long long
+using namespace std;
+int main()
+{
+
+}
